4d98d54e7d7203a2a4bf343317efae45a3edcf01
vermaasv833 <vermaasv833@gmail.com>
2025-01-11 03:19:13 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
+const BOT_TOKEN = "8166257595:AAGF_SIVXRmXTWeLo88H9f8bIT3HkS_cdlY"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
+const BOT_OWNER = 7825674005; // Insert your telegram account id.
+const BOT_CHANNEL = -1002313368299; // Insert your telegram channel id which the bot is admin in.
 const SIA_NUMBER = 12345; // Insert a random integer number and keep it safe.
 const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
 
